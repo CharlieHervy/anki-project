9840a638740d60e20a9952f934f5d79919690245
CharlieHervy <charlie@hervy.se>
2026-05-17 22:44:56 +0200

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://anki-project-three.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
